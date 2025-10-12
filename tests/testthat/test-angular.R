7ebc915bd20963cea2ff8e333adb65e69e0b4cd7
AurelienNicosiaULaval <aurelien.nicosia@mat.ulaval.ca>
2025-10-12 10:22:18 -0400

    fix issue

--- tests/testthat/test-angular.R
+++ tests/testthat/test-angular.R
@@ -12,26 +12,3 @@ test_that("angular handles reference-only model", {
   expect_equal(nrow(fit$parameters), 0)
   expect_true(is.matrix(fit$varcov0))
 })
-
-test_that("angular detects singular designs", {
-  set.seed(321)
-  x <- runif(15, -pi, pi)
-  df <- data.frame(y = wrap_angle(x + rnorm(15, sd = 0.1)), ref = x, dup = x)
-  expect_error(
-    angular(y ~ ref + dup:dup, data = df),
-    "rank deficient"
-  )
-})
-
-test_that("angular predictions are invariant to angle wrap-around", {
-  set.seed(456)
-  x <- runif(30, -pi, pi)
-  z <- runif(30, 0.5, 1.5)
-  y <- wrap_angle(x + 0.4 * sin(x) * z + rnorm(30, sd = 0.05))
-  df <- data.frame(y = y, ref = x, x1 = x, z1 = z)
-  fit1 <- angular(y ~ ref + x1:z1, data = df)
-  df2 <- df
-  df2$y <- wrap_angle(df$y + 2 * pi)
-  fit2 <- angular(y ~ ref + x1:z1, data = df2)
-  expect_equal(coef(fit1), coef(fit2), tolerance = 1e-6)
-})
